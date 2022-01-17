d0276a4ebdc24feb810c12c335514507799b6092
igoriev36 <igoryev26@gmail.com>
2022-01-17 11:41:53 -0800

    chore: switch every() to forEach() in test

--- test/index.js
+++ test/index.js
@@ -6,7 +6,7 @@ const { search } = require('../index.js')
 
 const checkResults = (results) => {
   assert.ok(results.length > 0)
-  results.every((val) => {
+  results.forEach((val) => {
     assert.ok(/^\d+$/.test(val.ref))
     assert.strictEqual(typeof val.score, 'number')
     assert.ok(typeof val.matchData, 'object')
